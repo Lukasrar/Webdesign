cea125625d7f1f67cf5ca8152601e1f81b75c6d4
lukas <lukasrar001@gmail.com>
2018-09-11 15:15:07 -0300

    att calculadora

---
+++ lista7/lista07.js
@@ -0,0 +1,16 @@
+function Calculadora1(operando1, operando2) {
+  this.operando1 = operando1;
+  this.operando2 = operando2;
+  this.somar = function() {
+    return this.operando1 + this.operando2;
+  }
+  this.subtrair = function() {
+    return this.operando1 - this.operando2;
+  }
+  this.multiplicar = function() {
+    return this.operando1 * this.operando2;
+  }
+  this.dividir = function() {
+    return this.operando1 / this.operando2;
+  }
+}
