894cdeb595bccb5664adeb3b1732754da99e32e4
lukas <lukasrar001@gmail.com>
2018-09-16 17:42:44 -0300

    finalizando js e css

--- lista7/lista07.js
+++ lista7/lista07.js
@@ -1,6 +1,6 @@
-function Calculadora1(operando1, operando2) {
-  this.operando1 = operando1;
-  this.operando2 = operando2;
+function Calculadora1(operando1Final, operando2Final) {
+  this.operando1 = operando1Final;
+  this.operando2 = operando2Final;
   this.somar = function() {
     return this.operando1 + this.operando2;
   }
@@ -12,5 +12,14 @@ function Calculadora1(operando1, operando2) {
   }
   this.dividir = function() {
     return this.operando1 / this.operando2;
+  }
+  this.raiz = function() {
+    return Math.sqrt(this.operando1); 
+  }
+  this.mod = function() {
+    return this.operando1 % this.operando2; 
+  }
+  this.div = function() {
+    return this.operando1 / this.operando2; 
   }
 }
