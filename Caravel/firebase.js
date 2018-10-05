4b8f46d3526814f851298ef577b1b4f325b1a1b5
lukas <lukasrar001@gmail.com>
2018-10-05 17:08:27 -0300

    Att organizacao nos comentários

--- Caravel/firebase.js
+++ Caravel/firebase.js
@@ -1,4 +1,4 @@
-// Initialize Firebase
+// Iniciando o FireBase
 var config = {
     apiKey: "AIzaSyCY7bbHOn3LbWRPZuiyeqJeCjaKvpAgL3c",
     authDomain: "contactformcaravel.firebaseapp.com",
@@ -9,27 +9,38 @@ var config = {
 };
 firebase.initializeApp(config);
 
+// Referencia das mensagens
 var mensagens = firebase.database().ref('mensagens')
 
-
-
+// Pegando valor dos inputs
 function getInput(campo){
     return document.getElementById(campo).value;
 }
+
+//Listener de clicks
 $('formularioDeContato').addEventListener('submit', enviarFormulario);
+
+//Enviar formulario
 function enviarFormulario(e) {
     e.preventDefault();
+
+    //Valores
     var nome = getInput('nome');
     var email = getInput('email');
     var mensagem = getInput('mensagem');
 
+    //Salvar mensagem no Firebase
     salvar(nome, email, mensagem);
 
+    //Alerta de sucesso
     $('sucesso').style.left = '0';
+
+    //Retirada do alerta
     setTimeout(function () {
         $('sucesso').style.left = '-200%';
     }, 3000)
 
+    //Resetando o formulário após o envio
     $('formularioDeContato').reset();
 }
 
--- Caravel/funcoes.js
+++ Caravel/funcoes.js
@@ -1,29 +1,31 @@
+//Zerar font-size dos icones do footer
 function start() {
     $('icone1').style.fontSize = '0px';
     $('icone2').style.fontSize = '0px';
     $('icone3').style.fontSize = '0px';
     $('icone4').style.fontSize = '0px';
 }
+//Abrir o menu lateral
 function openSlideMenu(){
     $('side-menu').style.width = '350px';
 }
+//Fechar menu lateral
 function closeSlideMenu(){
     $('side-menu').style.width = '0';
 }
+//Abrir icone no footer
 function abrirFooter1() {
     $('icone1').style.fontSize = '20px';
 }
 function fecharFooter1(){
     $('icone1').style.fontSize = '0px';
 }
-
 function abrirFooter2() {
     $('icone2').style.fontSize = '20px';
 }
 function fecharFooter2() {
     $('icone2').style.fontSize = '0px';
 }
-
 function abrirFooter3() {
     $('icone3').style.fontSize = '20px';
 }
@@ -31,28 +33,22 @@ function fecharFooter3() {
     $('icone3').style.fontSize = '0px';
 }
 
+//Ativar animate.css nos campos que foram submetidos
 function animateTest(idCampo) {
     $(idCampo).classList.add('headShake');
     var timer = setTimeout(function () {
         $(idCampo).classList.remove('headShake')
     },1000)
 }
-
+//Ativar animate.css nos campos que foram submetidos
 function animateUl(idCampo) {
     $(idCampo).classList.add('flipInY');
     var timer = setTimeout(function () {
         $(idCampo).classList.remove('flipInY')
     },500)
 }
-function logoAnimation(idCampo) {
-    $(idCampo).classList.add(' flipInY');
-    var timer = setTimeout(function () {
-        $(idCampo).classList.remove('flipInY')
-    },500)
-}
 
-
-
+//Função que retorna document.getElementById
 function $(campo){
     return document.getElementById(campo);
 }
